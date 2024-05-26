e02e2b765fd1710a3fdd4edc9dcfa35dae495172
Mastersam07 <abadasamuelosp@gmail.com>
2024-05-26 23:50:40 +0100

    :ambulance: chore: upgrade to dynamic loading

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 if (figma.editorType === 'figma' || figma.editorType === 'dev') {
     figma.showUI(__html__, { width: 600, height: 600 });
 
-    figma.ui.onmessage = msg => {
+    figma.ui.onmessage = async msg => {
         if (msg.type === 'generate-textstyles') {
             const useThemeExtensions = msg.useThemeExtensions;
             const includeFontName = msg.includeFontName;
-            let dartCode = generateTextStyles(useThemeExtensions, includeFontName);
+            let dartCode = await generateTextStyles(useThemeExtensions, includeFontName);
             figma.ui.postMessage({ type: 'dart-code', code: dartCode });
         }
 
         if (msg.type === 'generate-colors') {
-            let dartCode = generateColors();
+            let dartCode = await generateColors();
             figma.ui.postMessage({ type: 'dart-code', code: dartCode });
         }
 
         if (msg.type === 'generate-effects') {
-            let dartCode = generateEffectStyles();
+            let dartCode = await generateEffectStyles();
             figma.ui.postMessage({ type: 'dart-code', code: dartCode });
         }
     };
